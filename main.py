af24bac77c4e7a8a75b6579e1d96fd80edc1f641
Andreuwsh <127367293+Andreuwsh@users.noreply.github.com>
2023-08-29 14:30:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def start(message):
     bot.send_message(message.chat.id, text = 'Hello user! I am the most common Telegram bot that can send cats for all occasions! In order to get your cat, just write to the chat with the bot "Send a cat", "Cat".')
 
 @bot.message_handler(content_types = ['text'] )
-def ping(message):
+def cat(message):
     if 'Send a cat' in message.text or 'send a cat' in message.text or 'Cat' in message.text or 'cat' in message.text:
          response = requests.get('https://api.thecatapi.com/v1/images/search')
          cat_url = response.json()[0]['url']
